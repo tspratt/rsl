1ac120cba31ca58292bb3e8bef3959f0e807211f
Tracy <tspratt@gmail.com>
2015-11-03 19:56:04 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('winston');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var model = require('./model');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+var uri = process.env.MONGOLAB_URI;
+model.initDb(uri, function(err, db){
+  app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+  });
+});
+
+module.exports = app;
---
+++ model.js
@@ -0,0 +1,141 @@
+/**
+ * this file does the work of communicating with the Mongo database.
+ */
+'use strict';
+
+var StatusResponse = require('./lib/statusResponse').StatusResponse;
+var winston = require('winston');
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)({'timestamp': true, level: 'info'})
+    ]
+});
+
+//var async = require('async');
+
+var mongoDb = require('mongodb');
+var ObjectId = require('mongodb').ObjectID;
+var _db;
+
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({});
+
+
+function initDb(uri, callback) {
+    logger.info('model.initDb');
+    var statusResponse;
+    mongoDb.MongoClient.connect(uri,function (err, db) {
+        if(err) {
+            statusResponse = new StatusResponse('error', "System Error, please try again", '', null, err);
+            logger.error(JSON.stringify(statusResponse));
+            callback(err, {modelName: 'mgoModel', dbName: 'shades'});
+        }
+        else {
+            _db = db;
+            callback(err, {modelName: 'model', dbName: ''});
+        }
+    })
+}//
+
+/*
+function listMembers(filterSpec, pageSpec, oFieldSpec, callback){
+    var oMember;
+    db.collection('members', {safe: true},
+      function(err, collection){
+          var iSkip = 0;
+          var iLimit = 0;
+          if (pageSpec) {
+              iSkip = pageSpec.pageNum * pageSpec.pageLength;
+              iLimit = pageSpec.pageLength;
+          }
+          var oQuery = {};
+          if (filterSpec) {
+              var sQuery = '{"' + filterSpec.field + '":"' + filterSpec.value + '"}';
+              oQuery = JSON.parse(sQuery);
+          }
+          var aMembers = collection.find(oQuery, oFieldSpec)
+            .skip(iSkip)
+            .limit(iLimit)
+            .toArray(function (err, data) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    callback(null, data);
+                }
+            });
+      });
+}
+
+function filterMembersByName(matchString, oFieldSpec, callback){
+    var oMember;
+    db.collection('members', {safe: true},
+      function(err, collection){
+          var aMembers = collection.find({$or:[
+            {"first_name": new RegExp(matchString,'i')},
+            {"last_name": new RegExp(matchString,'i')}
+          ]}, oFieldSpec)
+            .toArray(function (err, data) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    callback(null, data);
+                }
+            });
+      });
+}
+*/
+
+/**
+ * run only once for a new collection!
+ */
+
+function parseJson () {
+    var sCollection = 'members';
+    converter.on("end_parsed", function (jsonArray) {
+        console.log(jsonArray); //here is your result jsonarray
+        insertData(sCollection, jsonArray);
+    });
+    require("fs").createReadStream('./data/' + sCollection + '.csv').pipe(converter);
+}
+function insertData(sCollection, json){
+    _db.collection(sCollection, {safe: true},
+      function(err, collection){
+          collection.insert(json, function (err, data) {
+              if (err) {
+                  callback(err, null);
+              } else {
+                  callback(null, data);
+              }
+          });
+      });
+
+}
+
+
+function getMember(id, callback){
+  var oMember;
+  db.collection('members', {safe: true},
+    function(err, collection){
+      var oId = new ObjectId(id);
+      collection.findOne({_id: oId},function (err, data) {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, data);
+        }
+      });
+    });
+
+}
+
+
+
+
+//exports.getMember = getMember;
+//exports.filterMembersByName = filterMembersByName;
+//exports.listMembers = listMembers;
+exports.db = _db;
+exports.initDb = initDb;
+//exports.insertMembers = insertMembers;
+
+
