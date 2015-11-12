f9368670728252c48095f1e76881f2e1e64d51e1
Tracy <tspratt@gmail.com>
2015-11-12 12:17:05 -0500

    Formatting Person detail form

--- public/controllers/person-list.js
+++ public/controllers/person-list.js
@@ -4,53 +4,17 @@ angular.module('rsl')
       $scope.persons = [];
       $scope.person = {};
       $scope.selectedId = '';
+      $scope.loggedInPersonId = '563c1e35ef69c27818dd916d';
       var totalCount = 100;
 
       $scope.shareToPercent = function(share) {
         return Math.round(share * 1000000)/10000;
       };
 
-      //paginator code: TODO: move into directive
-      $scope.pageNum = 0;
-      $scope.pageLen = "10";
-
       function initModule(){
         getPersons();
       }
 
-      $scope.nextPage = function (){
-        console.log('nextPage');
-        var iPageLen = parseInt($scope.pageLen);
-        if ((iPageLen * ($scope.pageNum+1)) < totalCount){
-          $scope.pageNum ++;
-          getPersons();
-        }
-
-      };
-      $scope.prevPage = function (){
-        console.log('prevPage');
-        if ($scope.pageNum > 0) {
-          $scope.pageNum --;
-          getPersons();
-        }
-      };
-      $scope.firstPage = function (){
-        console.log('firstPage');
-        $scope.pageNum = 0;
-        getPersons();
-      };
-      $scope.lastPage = function (){
-        var iPageLen = parseInt($scope.pageLen);
-        $scope.pageNum = Math.floor(totalCount / iPageLen) - 1;
-        getPersons();
-      };
-
-      $scope.onChangePageLen = function(){
-        console.log('onChangePageLen');
-        $scope.pageNum = 0;
-        getPersons();
-      };
-      //END paginator code
 
       function getPersons () {
         $scope.person = null;
@@ -59,9 +23,6 @@ angular.module('rsl')
           .then(function (res) {
             if (res.status >= 200 && res.status < 300) {
               $scope.persons = res.data.data;
-              if ($scope.persons.length === 1) {
-                $scope.person = $scope.persons[0];
-              }
             }
             else {
               console.log('HTTP Error: ' + res.statusText);
@@ -70,11 +31,10 @@ angular.module('rsl')
           });
       }
 
-
-      $scope.onClickPerson = function(oPerson) {
-        $scope.person = oPerson;
-        $scope.selectedId = oPerson._id
-      };
+      //$scope.onClickPerson = function(oPerson, index) {
+      //  $scope.person = oPerson;
+      //  $scope.selectedId = oPerson._id
+      //};
 
       initModule();
 
