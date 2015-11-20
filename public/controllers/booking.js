827dd9f299c7428d7b7d7750a1e7ec51c4372412
Tracy.Spratt <tspratt@consultant.underarmour.com>
2015-11-19 23:22:24 -0500

    Working Booking

--- public/controllers/booking.js
+++ public/controllers/booking.js
@@ -2,6 +2,11 @@ angular.module('rsl')
   .controller('bookingCtrl', ['$scope', '$state', 'appConstants', 'appData', 'bookingData', 'PersonData',
     function($scope, $state, appConstants, appData, bookingData, PersonData) {
       $scope.rooms = [];
+			$scope.selectedRoom = null;
+
+			$scope.arriveDetail = false;	//controls visibility of detail section
+			$scope.departDetail = false;
+
       function initModule(){
         //getBookings();
         getRooms();
@@ -26,10 +31,20 @@ angular.module('rsl')
       }
 
       function getRooms () {
-        bookingData.getRooms(null, null, null, {field: 'unit', value: appData.loggedInUser.person.member.branch.unit}, null)
+        bookingData.getRooms(null, null, 'unit', appData.loggedInUser.person.member.branch.unit, null)
             .then(function (res) {
               if (res.status >= 200 && res.status < 300) {
                 $scope.rooms = res.data.data;
+								var defaultRoomId = appData.loggedInUser.person.member.defaultroom;
+								if(defaultRoomId) {
+									for (var i = 0; i< $scope.rooms.length; i++) {
+										if ($scope.rooms[i]._id === defaultRoomId) {
+											$scope.selectedRoom =  $scope.rooms[i];
+											break;
+										}
+									}
+								}
+
               }
               else {
                 console.log('HTTP Error: ' + res.statusText);
